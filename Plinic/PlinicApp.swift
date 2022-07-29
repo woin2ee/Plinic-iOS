fea340a282c38e010fce12a777ca3660a751aa9c
blooper20 <yjw127588@gmail.com>
2022-07-29 14:19:56 +0900

    Initial Commit

---
+++ Plinic/PlinicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlinicApp.swift
+//  Plinic
+//
+//  Created by MacBook Air on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct PlinicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
