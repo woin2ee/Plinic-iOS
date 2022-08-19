54e6205862f722a79a7212dc8d72f625d399feb5
blooper20 <yjw127588@gmail.com>
2022-08-19 17:03:54 +0900

    :sparkles: (jaewoo/feature/postDetail/#27) 게시글 상세 페이지 구현

--- Plinic/user/userPlaylist.swift
+++ Plinic/user/userPlaylist.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct UserPlaylist: View {
+    
+    var playlistURL : String
+    
     var body: some View {
         ZStack {
             Color.black
@@ -15,7 +18,7 @@ struct UserPlaylist: View {
             VStack{
                 PlaylistInfo(playlistTitle: "자기전에 듣기 좋은 노래", songs: "20")
                 
-                PlaylistWebView(playlistURL: "https://www.youtube.com/watch?v=vadat46yfwg&list=PL7-f8-kndz_d11Enm7ttqXKfqSKzvG60Q&index=1")
+                PlaylistWebView(playlistURL: "\(playlistURL)")
                     .frame(height: 550)
             } // VStack
         } // ZStack
@@ -24,7 +27,7 @@ struct UserPlaylist: View {
 
 struct UserPlaylist_Previews: PreviewProvider {
     static var previews: some View {
-        UserPlaylist()
+        UserPlaylist(playlistURL: "https://www.youtube.com/watch?v=vadat46yfwg&list=PL7-f8-kndz_d11Enm7ttqXKfqSKzvG60Q&index=1")
     }
 }
 
