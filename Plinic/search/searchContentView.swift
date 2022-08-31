c5afbd8c7e2270fc4bddbc2114a151fb91d33284
blooper20 <yjw127588@gmail.com>
2022-08-31 20:13:31 +0900

    :construction: (jaewoo/feature/searchPage/#34) 둘러보기 구현 및 search Bar  기본 구현

---
+++ Plinic/search/searchContentView.swift
@@ -0,0 +1,50 @@
+//
+//  searchContentView.swift
+//  Plinic
+//
+//  Created by MacBook Air on 2022/08/30.
+//
+
+import SwiftUI
+
+struct SearchContentView: View {
+    
+    var genre = ["Happy", "Sad", "Jazz", "Acoustic", "Blues", "Classical", "Children", "R&B"]
+    @State private var searchText = ""
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack{
+                NavigationView {
+                    Text("Searching for \(searchText)")
+                        .searchable(text: $searchText, prompt: "검색어를 입력하세요.")
+                } // SearchBar
+                
+                ScrollView{
+                    VStack{
+                        ForEach( Array(stride(from: 0, to: self.genre.count, by: 2)), id: \.self) { index in
+                            HStack{
+                                GenreThumbnail(genreImg: "defaultImg", genreName: "\(genre[index])")
+                                if genre[index+1].isEmpty {
+                                    //                                (이때, genre안에 있는 요소가 짝수가 아닌 홀수 일때에 오류가 남) <- 오류처리 필요
+                                }
+                                else {
+                                    GenreThumbnail(genreImg: "defaultImg", genreName: "\(genre[index+1])")
+                                }
+                            } // HStack
+                        } //Foreach 0부터 시작해서 2의 간격으로 index를 출력 (이때, genre안에 있는 요소가 짝수가 아닌 홀수 일때에 오류가 남) <- 오류처리 필요
+                    } // VStack
+                } // ScrollView
+            }
+            
+        } // ZStack
+    }
+}
+
+struct SearchContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchContentView()
+    }
+}
