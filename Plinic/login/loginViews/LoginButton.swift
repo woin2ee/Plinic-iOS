9cd63c3817cf86ed87efca8cde81b315dd50c9b6
blooper20 <yjw127588@gmail.com>
2022-10-11 20:33:16 +0900

    :construction: :poop: (jaewoo/feature/KakaoLogin/#48) 카카오 로그인 구현 중 오류 발생

--- Plinic/login/loginViews/LoginButton.swift
+++ Plinic/login/loginViews/LoginButton.swift
@@ -8,47 +8,22 @@
 import Foundation
 import SwiftUI
 
+//import KakaoSDKCommon
+//import KakaoSDKAuth
+
 struct LoginButton : View {
     
-    
-    @State var nameInput: String = ""
-    @State var emailInput: String = ""
-    @State var passwordInput: String = ""
-    @State var passwordConfirmInput: String = ""
-    
-    init(){
-        UITableView.appearance().backgroundColor = .clear
-    }
-    
+//    @UIApplicationDelegateAdaptor var appDelegate : MyAppDelegate
     var body: some View{
         
-        
-        
-        VStack{
-            Form {
-                Section(header: Text("이름")) {
-                    TextField("이름을 입력해주세요", text: $nameInput).keyboardType(.default).autocapitalization(.none)
-                }
-                .listRowBackground(Color.white.opacity(0.7))
-                Section(header: Text("이메일")) {
-                    TextField("이메일", text: $emailInput).keyboardType(.emailAddress).autocapitalization(.none)
-                }
-                .listRowBackground(Color.white.opacity(0.7))
-                Section(header: Text("비밀번호")) {
-                    SecureField("비밀번호", text: $passwordInput).keyboardType(.default)
-                    SecureField("비밀번호 확인", text: $passwordConfirmInput).keyboardType(.default)
-                }
-                .listRowBackground(Color.white.opacity(0.7))
-                Section{
-                    Button {
-                        print("회원가입 버튼 클릭")
-                    } label: {
-                        Text("회원가입하기")
-                    }
-                }
-                .listRowBackground(Color.white.opacity(0.7))
-            }
-        }// VStack
+        Button(action: {
+            
+        }, label: {
+            Text("카카오 로그인")
+//            Image("kakao_login_medium_wide")$
+//                .resizable()
+//                .aspectRatio(contentMode: .fill)
+        })
     }
     
 }
