949e04b82dcc2a7ad3eac2a4808409ff2ee3c652
Jaewon-Yun <81426024+woin2ee@users.noreply.github.com>
2022-10-27 11:46:56 +0900

    Update Plinic/Views/Search/SearchViews/MoreUserResultsView.swift

--- Plinic/Views/Search/SearchViews/MoreUserResultsView.swift
+++ Plinic/Views/Search/SearchViews/MoreUserResultsView.swift
@@ -23,7 +23,7 @@ struct MoreUserResultsView: View {
                     ForEach(users, id: \.uuid) { user in
                         NavigationLink(destination: OtherUserView(nickName: user.nickName)){
                             HStack{
-                                AsyncImage(url: URL(string: user.profileImageUrl ?? "플리닉로고")){ image in
+                                AsyncImage(url: URL(string: user.profileImageUrl)){ image in
                                     image
                                         .resizable()
                                         .clipShape(Circle())
