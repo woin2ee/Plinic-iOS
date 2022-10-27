94bda28efba3a7c8f455776e3593edb36559e631
blooper20 <yjw127588@gmail.com>
2022-10-27 21:52:27 +0900

    :recycle: (jaewoo/release) 전체적인 네비게이션 바 작업

--- Plinic/Views/User/OtherUserView.swift
+++ Plinic/Views/User/OtherUserView.swift
@@ -17,54 +17,58 @@ struct OtherUserView: View {
     @State var nickName: String
     
     var body: some View {
-        NavigationView(){
-            ZStack {
+        
+        ZStack {
+            
+            Color.black
+                .ignoresSafeArea()
+            GeometryReader{ geometry in
                 
-                Color.black
-                    .ignoresSafeArea()
-                    GeometryReader{ geometry in
-                        
-                        VStack{
-                            UserInfoView(userInfo: $userInfo)
-                                .frame(height: geometry.size.height * 0.25)
-                                .padding(.top, 10)
-                            // 유저 정보 부분
-                            if(self.postTransform) {
-                                OtherUserPlaylistView(userInfo: $userInfo)
-                                    .frame(height: geometry.size.height * 0.65)
-                                Spacer()
-                                    .frame(height: geometry.size.height * 0.15)
-                            } else {
-                                UserPostView(userInfo: $userInfo)
-                                    .frame(height: geometry.size.height * 0.65)
-                            }
-                            
-                        } // VStack
-                    } // GeometryReader
-                    
-            } // ZStack
-            .onAppear() {
-                userAPI.getUserInfo(by: nickName) { result in
-                    switch result {
-                    case .success(let userInfo):
-                        self.userInfo = userInfo
-                    case .failure(let error):
-                        print(error)
+                VStack{
+                    HStack{
+                        Text(userInfo.nickName)
+                            .foregroundColor(Color.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 30))
+                            .padding(.leading, 10)
+                        Spacer()
+                        Button(action: {
+                            self.postTransform.toggle()
+                        }, label: {
+                            Image(systemName: "music.note.list")
+                                .padding(.trailing, 10.0)
+                                .font(.system(size: 44))
+                                .foregroundColor(Color.MainColor)
+                        })
                     }
+                    UserInfoView(userInfo: $userInfo)
+                        .frame(height: geometry.size.height * 0.25)
+                        .padding(.top, 10)
+                    // 유저 정보 부분
+                    if(self.postTransform) {
+                        OtherUserPlaylistView(userInfo: $userInfo)
+                            .frame(height: geometry.size.height * 0.65)
+                        Spacer()
+                            .frame(height: geometry.size.height * 0.15)
+                    } else {
+                        UserPostView(userInfo: $userInfo)
+                            .frame(height: geometry.size.height * 0.65)
+                    }
+                    
+                } // VStack
+            } // GeometryReader
+            
+        } // ZStack
+        .onAppear() {
+            userAPI.getUserInfo(by: nickName) { result in
+                switch result {
+                case .success(let userInfo):
+                    self.userInfo = userInfo
+                case .failure(let error):
+                    print(error)
                 }
             }
         }
-        .navigationTitle(userInfo.nickName)
-        .navigationBarItems(trailing:
-                                Button(action: {
-            self.postTransform.toggle()
-        }, label: {
-            Image(systemName: "music.note.list")
-                .padding(.trailing, 10.0)
-                .font(.system(size: 20))
-                .foregroundColor(Color.MainColor)
-        }))
-        
         
     }
 }
