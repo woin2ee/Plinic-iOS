e088c64242fcc1a6e8f0c42dcea7d3fc1210068c
Jaewon <woin2ee@gmail.com>
2022-10-30 01:25:01 +0900

    :recycle: (jaewon/refactor) Extension 파일 네이밍 방식 통일

--- Plinic/Extensions/View+.swift
+++ Plinic/Extensions/View+.swift
@@ -12,4 +12,9 @@ extension View {
     func onLoad(perform action: (() -> Void)? = nil) -> some View {
         return modifier(ViewDidLoadModifier.init(perform: action))
     }
+    
+    /// 다른 부분 탭했을 때 키보드 숨기는 함수
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
